e9ce703438f07da1f9e8899fb4ec1985602ac79f
yonas44 <72081991+yonas44@users.noreply.github.com>
2022-03-12 18:29:47 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-int i;
-int j;
-int k;
+	int i;
+	int j;
+	int k;
 
-for (i = 48; i < 56; i++)
-{
-for (j = i + 1; j < 57; j++)
-{
-for (k = j + 1; k < 58; k++)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (i != 55 || j != 56 || k != 57)
-{
-putchar(,);
-putchar( );
-}
-}
-}
-}
-putchar(n);
-return (0);
+	for (i = 48; i < 56; i++)
+	{
+		for (j = i + 1; j < 57; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }

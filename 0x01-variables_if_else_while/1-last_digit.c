a3017be575a095446eb172c836330ee3324052ff
yonas44 <72081991+yonas44@users.noreply.github.com>
2022-03-12 17:52:09 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,20 +8,20 @@
  */
 int main(void)
 {
-0-positive_or_negative.c int n;
-0-positive_or_negative.c int L;
-0-positive_or_negative.c char str[] = "Last digit of";
+	int n;
+	int L;
+	char str[] = "Last digit of";
 
-0-positive_or_negative.c srand(time(0));
-0-positive_or_negative.c n = rand() - RAND_MAX / 2;
-0-positive_or_negative.c L = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
 
-0-positive_or_negative.c if (L > 5)
-0-positive_or_negative.c 0-positive_or_negative.c printf("%s %d is %d and is greater than 5\n", str, n, L);
-0-positive_or_negative.c else if (L == 0)
-0-positive_or_negative.c 0-positive_or_negative.c printf("%s %d is %d and is 0\n", str, n, L);
-0-positive_or_negative.c else if (L < 6)
-0-positive_or_negative.c 0-positive_or_negative.c printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+	if (L > 5)
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
+	else if (L == 0)
+		printf("%s %d is %d and is 0\n", str, n, L);
+	else if (L < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 
-0-positive_or_negative.c return (0);
+	return (0);
 }

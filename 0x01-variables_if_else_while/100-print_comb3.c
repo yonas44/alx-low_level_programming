d191567bc31ff7c7d5832020d01707280df1eed1
yonas44 <72081991+yonas44@users.noreply.github.com>
2022-03-12 18:29:13 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-int i;
-int j;
+	int i;
+	int j;
 
-for (i = 48; i < 57; i++)
-{
-for (j = i + 1; j < 58; j++)
-{
-putchar(i);
-putchar(j);
-if (i != 56 || j != 57)
-{
-putchar(,);
-putchar( );
-}
-}
-}
-putchar(n);
-return (0);
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }

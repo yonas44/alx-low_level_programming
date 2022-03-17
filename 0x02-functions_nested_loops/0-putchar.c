5a4b353a71b35ff688874b69784e51b265d9e93e
root <tesfu.yonas1350@gmail.com>
2022-03-17 07:24:41 -0700

    another commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
 #include "main.h"
+/**
+ *main - writing a program that prints _putchar followed by a new line.
+ *Return: The value 0 is returned.
+ */
 int main(void)
 {
-  _putchar(_);
-  _putchar(p);
-  _putchar(u);
-  _putchar(t);
-  _putchar(c);
-  _putchar(h);
-  _putchar(a);
-  _putchar(r);
-  return (0);
+_putchar(_);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+return (0);
 }

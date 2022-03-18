590948a1da1b5d4200f9c0ba72d91768cbf97e29
yonas44 <tesfu.yonas1350@gmail.com>
2022-03-18 11:12:48 -0700

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_isalpha - function that checks for alphabetic characters
+ *
+ *@c: evaluating c and inserting it through the conditional
+ *
+ *main - checks for lowercase or uppercase letters.
+ *Return: 1 if c is a uppercase or lowercase
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_abs - absolute value
+ *
+ *@r: evalutates the value stored in x through the conditional
+ *
+ *main - printing the absolute value of a number
+ *
+ *Return: The value will return 0
+ */
+
+int _abs(int r)
+
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}

b6f34051ac5585b80d3e0b3c1caeaca20e8ae464
yonas44 <tesfu.yonas1350@gmail.com>
2022-03-18 23:42:11 -0700

    new commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - printing numbers up to or down until 98
+ *
+ *@n: This variable is used to compare certain conditions.
+ *
+ *Return: value will return void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
+}

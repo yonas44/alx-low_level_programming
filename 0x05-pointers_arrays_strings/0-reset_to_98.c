73413cc7d41f85482bfad878aea2fcf796c68f01
yonas44 <tesfu.yonas1350@gmail.com>
2022-03-20 23:51:59 -0700

    another commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
  * reset_to_98 - Entry point
  *@n: pointer the integer we want to set to 98
  * Return: nothing
  */
-
-void reset_to_98(int *n) 
+void reset_to_98(int *n)
 {
-	*n = 98;
 
+	*n = 98;
 }

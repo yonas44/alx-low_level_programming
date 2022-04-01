a4e86cafb9d501e68f5f12c1af1fa875896695a6
yonas44 <tesfu.yonas1350@gmail.com>
2022-04-01 10:28:01 -0700

    another commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -12,9 +14,11 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(c
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
